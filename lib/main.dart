d20eb42be0466830c87f23d62d6dd22be7a32911
artemboo <artem.filippov@nitrix.io>
2023-05-30 00:59:45 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tasty_cook/firebase_options.dart';
 import 'package:tasty_cook/init/app_init.dart';
 import 'package:tasty_cook/screens/home.dart';
 
@@ -7,6 +9,10 @@ import 'services/localization_service/localization_service.dart';
 
 void main() async {
   await AppInit.appInit();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     LocalizationService.localizationWidget(
